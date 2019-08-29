8d1875d20029fa26116f3292c08eda5aa75a2ae0
Yang Liu <liuyang12@focusmedia.cn>
2019-08-29 14:59:31 +0800

    udpate

--- extract.py
+++ extract.py
@@ -1,13 +1,13 @@
 import zipfile
 
 
-def extract(filename):
+def extract(filename, folder):
     print('Extracting {}...'.format(filename))
     tar = zipfile.ZipFile(filename, 'r')
-    tar.extractall('data')
+    tar.extractall('data/' + folder)
     tar.close()
 
 
 if __name__ == "__main__":
-    extract('data/vox1_dev_wav.zip')
-    extract('data/vox1_test_wav.zip')
+    extract('data/vox1_dev_wav.zip', 'train')
+    extract('data/vox1_test_wav.zip', 'valid')
