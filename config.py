ff44f0ffd083cca34ae73c6a7fdc234e55ee7f9f
Yang Liu <liuyang12@focusmedia.cn>
2019-08-30 10:09:04 +0800

    update

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ num_heads = 8
 
 # Training parameters
 grad_clip = 5.  # clip gradients at an absolute value of
-print_freq = 10  # print training/validation stats  every __ batches
+print_freq = 100  # print training/validation stats  every __ batches
 checkpoint = None  # path to checkpoint, None if none
 
 # Data parameters
