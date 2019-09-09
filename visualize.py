725a389685c8fdefd1cb557f772d22186d488396
Yang Liu <liuyang12@focusmedia.cn>
2019-09-09 14:27:02 +0800

    update

--- visualize.py
+++ visualize.py
@@ -2,13 +2,15 @@ import pickle
 from matplotlib import pylab
 import matplotlib
 import torch.nn.functional as F
+import random
 matplotlib.use('tkagg')
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
 from sklearn.manifold import TSNE
 from tqdm import tqdm
-
+import os
+from config import wav_folder
 import config as hp
 from models.embedder import GST
 from utils import extract_feature, build_LFR_features
@@ -21,15 +23,30 @@ if __name__ == '__main__':
     model = model.to(hp.device)
     model.eval()
 
-    with open(hp.data_file, 'rb') as file:
-        data = pickle.load(file)
-    samples = data['valid']
+    dirs = [d for d in os.listdir(wav_folder) if d.startswith('id')]
+    dirs = random.sample(dirs, 10)
 
-    embeddings = np.zeros((1000, 512), dtype=np.float)
+    samples = []
+
+    for id in dirs:
+        label = id
+        folder = os.path.join(wav_folder, id)
+        sub_folders = [s for s in os.listdir(folder)]
+        for sub in sub_folders:
+            sub_folder = os.path.join(folder, sub)
+            files = [f for f in os.listdir(sub_folder) if f.endswith('.wav')]
+            for f in files:
+                audiopath = os.path.join(sub_folder, f)
+                samples.append({'audiopath': audiopath, 'label': label})
+
+    num_samples = len(samples)
+    print('num_samples: ' + str(num_samples))
+
+    embeddings = np.zeros((num_samples, 512), dtype=np.float)
     dots = []
     labels = []
     with torch.no_grad():
-        for i in tqdm(range(len(samples))):
+        for i in tqdm(range(num_samples)):
             sample = samples[i]
             wave = sample['audiopath']
             label = sample['label']
