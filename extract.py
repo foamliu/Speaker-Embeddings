a1de7f0570428554dc024e58477a8a08216c9a73
Yang Liu <liuyang12@focusmedia.cn>
2019-09-10 09:03:39 +0800

    update

--- extract.py
+++ extract.py
@@ -1,13 +1,13 @@
 import zipfile
 
 
-def extract(filename):
+def extract(filename, split):
     print('Extracting {}...'.format(filename))
     tar = zipfile.ZipFile(filename, 'r')
-    tar.extractall('data/vox1')
+    tar.extractall('data/vox1/' + split)
     tar.close()
 
 
 if __name__ == "__main__":
-    extract('data/vox1_dev_wav.zip')
-    extract('data/vox1_test_wav.zip')
+    extract('data/vox1_dev_wav.zip', 'dev')
+    extract('data/vox1_test_wav.zip', 'test')
