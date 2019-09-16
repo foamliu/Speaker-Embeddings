5f25ad2ee21bf71c007fdb127490b164ab0fc92e
Yang Liu <liuyang12@focusmedia.cn>
2019-09-16 11:18:12 +0800

    update

--- visualize.py
+++ visualize.py
@@ -60,6 +60,10 @@ def get_annotations(two_d, ids):
         ys.append(speakers[id]['y'] / cnt)
         ids.append(id)
 
+    print('xs: ' + str(xs))
+    print('ys: ' + str(ys))
+    print('ids: ' + str(ids))
+
     return xs, ys, ids
 
 
